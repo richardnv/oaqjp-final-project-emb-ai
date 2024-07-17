1327349f90479bce65d556c1a7262034804cbb92
richardnv <richardnv@gmail.com>
2024-07-17 15:09:39 -0400

    incorporate the server error handler for blank text

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,24 @@
 # Importing the requests library
 import requests, json
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_to_analyze=None):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     jsonObj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=jsonObj, headers=headers)
+
+    if response.status_code == 400:
+        error_output = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,          
+            "dominent_emotion": None
+        }
+        return error_output
+
     emotion_scores = json.loads(response.text)['emotionPredictions'][0]['emotion']
     sorted_scores = dict(sorted(emotion_scores.items(), key=lambda x: x[1], reverse=True))
     dominent_emotion = list(sorted_scores)[0]
--- server.py
+++ server.py
@@ -1,21 +1,27 @@
-import json, requests
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route('/emotionDetection', methods=['POST'])
-def fetect_emotion():
-    data = requests.get_data().__getattribute__('q')
-    text_to_analyze = data['text']
-    emotion_detection_result = emotion_detector(text_to_analyze)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    text_to_analyze  = request.args.get('textToAnalyze')
+    emotion_response = emotion_detector(text_to_analyze)
+
+    if emotion_response['dominent_emotion'] == None:
+        return "Invalid text! Please try again!."
     
     output = f"For the given statement, the system serponse is"
-    output += f" 'anger': {emotion_scores['anger']},"
-    output += f" 'disgust': {emotion_scores['disgust']},"
-    output += f" 'fear': {emotion_scores['fear']},"
-    output += f" 'joy': {emotion_scores['joy']},"
-    output += f" 'sadness': {emotion_scores['sadness']}."          
-    output += f" The dominent emotion is <b>{dominent_emotion}</b>."
+    output += f" 'anger': {emotion_response['anger']},"
+    output += f" 'disgust': {emotion_response['disgust']},"
+    output += f" 'fear': {emotion_response['fear']},"
+    output += f" 'joy': {emotion_response['joy']},"
+    output += f" 'sadness': {emotion_response['sadness']}."          
+    output += f" The dominent emotion is <b>{emotion_response['dominent_emotion']}</b>."
         
     return output
